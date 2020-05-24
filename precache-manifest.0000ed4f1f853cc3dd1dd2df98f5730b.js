68c29e85bd2264766ccdfe41bc0b4f8539ee9480
Ian Baker <ian.baker@iinteractive.com>
2020-05-24 18:19:25 -0400

    Updates

---
+++ precache-manifest.0000ed4f1f853cc3dd1dd2df98f5730b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70e8118624d74457814ea00b58e4a0b",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "c7ee741f5de33fe85b55",
+    "url": "/todo-list/static/css/main.3b7cd97a.chunk.css"
+  },
+  {
+    "revision": "77b108ebbb0f6c6f4be0",
+    "url": "/todo-list/static/js/2.c0349eae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.c0349eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ee741f5de33fe85b55",
+    "url": "/todo-list/static/js/main.24f966a3.chunk.js"
+  },
+  {
+    "revision": "2b909f8b49d22c3e9c72",
+    "url": "/todo-list/static/js/runtime-main.5e1967fb.js"
+  }
+]);
